de77277cd0c1509d63652da3eb8eb911b3201bfa
work_penguin <d.sahakyan10@gmail.com>
2024-11-22 12:48:43 +0400

    telegram bot polling

--- run_bot.py
+++ run_bot.py
@@ -1,6 +1,7 @@
 import os
 import django
-
+import requests
+import time
 # Настройки Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CityMissingProducts.settings")
 django.setup()
@@ -8,4 +9,12 @@ django.setup()
 
 from main.bot import bot
 if __name__ == "__main__":
-    bot.polling(none_stop=True)
+    while True:
+        try:
+            bot.polling(none_stop=True, timeout=60)
+        except requests.exceptions.ReadTimeout:
+            print("ReadTimeout: Перезапуск бота...")
+            time.sleep(5)
+        except Exception as e:
+            print(f"Ошибка: {e}")
+            time.sleep(5)
